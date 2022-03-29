07b6c370a162891c50387b566ef086b277d63509
Vitalii Melnychuk <vitalii.melnychuk@farfetch.com>
2022-03-29 23:56:47 +0100

    chore: remove percentage

--- packages/core/src/services/reporters/clones.ts
+++ packages/core/src/services/reporters/clones.ts
@@ -4,8 +4,8 @@ export const getClonesReportBody = (
   title: string,
   statistic: TClonesStatistic
 ): string => {
-  const totalLines = `Total Lines: <b>${statistic.total.lines}%</b>`;
-  const duplicatedLines = `Duplicated Lines: <b>${statistic.total.duplicatedLines}%</b>`;
+  const totalLines = `Total Lines: <b>${statistic.total.lines}</b>`;
+  const duplicatedLines = `Duplicated Lines: <b>${statistic.total.duplicatedLines}</b>`;
 
   const description = `${totalLines}\n\n${duplicatedLines}`;
 
